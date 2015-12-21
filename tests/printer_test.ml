95d1a441e75dac6f23b833995cda4925d3615131
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-12-21 13:14:04 +0900

    Fix tests

--- tests/printer_test.ml
+++ tests/printer_test.ml
@@ -12,12 +12,12 @@ let test_sprint_type_text =
   let f1 = TyFun (tyint, tybool, tyint, tybool) in
   let f2 = TyFun (f0, tybool, f1, tybool) in
   let check_list = [
-    TyDyn, "?";
+    TyDyn, "*";
     tybool, "bool";
     tyint, "int";
-    f0, "?/?->?/?";
+    f0, "*/*->*/*";
     f1, "int/bool->int/bool";
-    f2, "(?/?->?/?)/bool->(int/bool->int/bool)/bool"
+    f2, "(*/*->*/*)/bool->(int/bool->int/bool)/bool"
   ]
   in
   List.map
