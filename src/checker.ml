12a428f489276744ab67a5aedd5d947282cdb2c2
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-12-31 16:47:26 +0900

    Fix check_consistency

--- src/checker.ml
+++ src/checker.ml
@@ -12,7 +12,7 @@ type check_mode =
 let rec check_consistency s t = match s, t with
 | (s, t) when s = t -> true (* TCRefl *)
 | (s, TyDyn) -> true (* TCUnR *)
-| (TyDyn, s) -> true (* TCUnR *)
+| (TyDyn, t) -> true (* TCUnR *)
 | (TyFun (s1, a1, t1, b1), TyFun (s2, a2, t2, b2)) ->
     check_consistency s1 s2 &&
     check_consistency a1 a2 &&
